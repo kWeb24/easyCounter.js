39b48efa58234331791eb8770b4a4b491d1cab04
Kamil Weber <kamilweber24@gmail.com>
2017-02-11 16:48:04 +0100

    Init: Version 0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,158 @@
 # easyCounter.js
-EasyCounter.js is a library to make working with animated counters easier. It provides lightweight API that allows you to easily create and manage counters on your webpage.
+
+EasyCounter.js is a library to make working with animated counters easier. It provides lightweight API that allows you to easily create and manage counters on your webpage. Requires jQuery.
+
+**For running exampels and documentation visit this page** [easyCounter.js docs and examples](http://kamilweber.pl/bundle/easycounterjs/example)
+
+With this library you can easily attach counter to any element on your website and fire it when user scroll to the target element. All settings are available via HTML custom attributes so you could simply start your journey with:
+
+```html
+<div class="easycounter" ec-min="0" ec-max="123456789" ec-duration="2000"></div>
+```
+
+and then:
+
+```javascript
+$('.easycounter').easyCounter({});
+```
+
+# Options
+
+EasyCounter.js comes with many configurable options to let you manage your counters both globaly and localy. Before you start you have to know that the html inline attributes like `ec-duration` will override the same value specified at initialization but only in their own element.
+
+## Aavailable initialization options
+
+Library may be initialized with options shown below:
+
+```javascript
+$('.easycounter').easyCounter({
+		'duration': 1000,
+		'delay': 500,
+		'decimals': 0,
+		'runonce': false,
+		'disableoverride': false,
+		'direction': 'asc',
+		'attrmin': 'ec-min',
+		'attrmax': 'ec-max',
+		'attrdecimals': 'ec-decimals',
+		'attrduration': 'ec-duration',
+		'attrdelay': 'ec-delay',
+		'attrdirection': 'ec-direction',
+		'attrrunonce': 'ec-run-once',
+		'attrdisableoverride': 'ec-disable-override'
+	});
+```
+
+* `duration` - Duration of animation in miliseconds,
+* `delay` - Time in miliseconds that must elapse before animation starts after element in in viewport.
+* `decimals` - Integer number of decimal places to show in animation. Note: Library by default if not set both decimals and inline decimal attribute will take highest decimal places from `attrmin` and `attrmax`
+* `runonce` - Have to be true or false. By defaults animation starts each time after element gets into viewport. If set to true, animation fire only once and stay in complete state.
+* `disableoverride` - Have to be true or false. Define if the library can override default element text before start or not. Note: Library by default overrides default element text with `attrmin` value so when delay is set start value will be visible. After set to true library will not change element text untill animation starts.
+* `direction` - Must be 'asc' or 'desc'. Defines animation direction, for 'asc' - count from lowest to highest val, for 'desc' - count from highest to lowest.
+* `attrmin` - HTML inline attribute for minimal animation value. Default `ec-min` Overrides initialization options.
+* `attrmax` - HTML inline attribute for maximal animation value. Default `ec-max` Overrides initialization options.
+* `attrdecimals` - HTML inline attribute for decimal places. Default `ec-decimals` Overrides initialization options.
+* `attrduration` - HTML inline attribute for duration time. Default `ec-duration` Overrides initialization options.
+* `attrdelay` - HTML inline attribute for animation delay time. Default `ec-delay` Overrides initialization options.
+* `attrdirection` - HTML inline attribute for animation direction. Default `ec-direction` Overrides initialization options.
+* `attrrunonce` - HTML inline attribute for runOnce flag. Default 1 Overrides initialization options.
+* `attrdisableoverride` - HTML inline attribute for disableOverride flag. Default `ec-disable-override` Overrides initialization options.
+
+# Events
+
+EasyCounter.js fires events each time when element isVisible flag or animation state change. Available events:
+
+```javascript
+	$(window).on( "ec-element-enter-screen", {}, function(event, el) {
+	});
+
+	$(window).on( "ec-element-leave-screen", {}, function(event, el) {
+	});
+
+	$(window).on( "ec-animation-start", {}, function(event, el) {
+	});
+
+	$(window).on( "ec-animation-end", {}, function(event, el) {
+	});
+```
+
+* `ec-element-enter-screen` - Fires once when element enters viewport. Element object given,
+* `ec-element-leave-screen` - Fires once when element enters viewport. Element object given,
+* `ec-animation-start` - Fires once when animation starts. After delay (if set). Element object given,
+* `ec-animation-start` - Fires once when animation is done. Element object given,
+
+# Examples
+
+For running exampels and documentation visit this page [easyCounter.js docs and examples](http://kamilweber.pl/bundle/easycounterjs/example)
+
+## Easy start
+
+Setup basic counter fired when user scrolls to element
+
+```html
+	<div class="easycounter" ec-min="0" ec-max="15000"></div>
+	<script type="text/javascript" src="../src/easyCounter.js"></script>
+	<script>
+		$('.easycounter').easyCounter({});
+	</script>
+```
+
+## Values going back
+
+Same as previous example but counter runs backward
+
+```html
+	<div class="easycounter" ec-min="0" ec-max="15000" ec-direction="desc"></div>
+	<script type="text/javascript" src="../src/easyCounter.js"></script>
+	<script>
+		$('.easycounter').easyCounter({});
+	</script>
+```
+
+## Decimals taken from element
+
+Same as first example but with decimal places
+
+```html
+	<div class="easycounter" ec-min="0" ec-max="1000.1234"></div>
+	<script type="text/javascript" src="../src/easyCounter.js"></script>
+	<script>
+		$('.easycounter').easyCounter({});
+	</script>
+```
+
+## Decimals override by inline attribute
+
+Same as previous example but with decimal places set in attribute
+
+```html
+	<div class="easycounter" ec-min="0" ec-max="1000.1234"></div>
+	<script type="text/javascript" src="../src/easyCounter.js"></script>
+	<script>
+		$('.easycounter').easyCounter({});
+	</script>
+```
+
+## Run once with delay
+
+Same as previous first example but fire only once with 5 seconds delay
+
+```html
+	<div class="easycounter" ec-min="0" ec-max="15000" ec-run-once="true" ec-delay="5000"></div>
+	<script type="text/javascript" src="../src/easyCounter.js"></script>
+	<script>
+		$('.easycounter').easyCounter({});
+	</script>
+```
+
+# License
+
+[MIT License](https://opensource.org/licenses/mit-license.html)
+
+Copyright 2017 [Kamil Weber](http://kamilweber.pl/)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ src/easyCounter.js
@@ -0,0 +1,231 @@
+/*!
+* @license easyCounterJS
+* Visit [https://github.com/kWeb24/easyCounter.js] for documentation, updates and examples.
+*
+* Copyright (c) 2017 kamilweber.pl
+*
+* Distributed under the terms of the MIT license.
+* http://www.opensource.org/licenses/mit-license.html
+*
+* This notice shall be included in all copies or substantial portions of the Software.
+*/
+
+;( function( $, window, document, undefined ) {
+
+	"use strict";
+		var pluginName = "easyCounter",
+			defaults = {
+				'duration': 1000,
+       	'delay': 0,
+				'decimals': 0,
+				'runonce': false,
+				'disableoverride': false,
+       	'direction': 'asc',
+       	'attrmin': 'ec-min',
+       	'attrmax': 'ec-max',
+				'attrdecimals': 'ec-decimals',
+				'attrduration': 'ec-duration',
+				'attrdelay': 'ec-delay',
+				'attrdirection': 'ec-direction',
+				'attrrunonce': 'ec-run-once',
+				'attrdisableoverride': 'ec-disable-override'
+			};
+
+		function Plugin ( element, options ) {
+			this._name = pluginName;
+			this.element = element;
+			this._defaults = defaults;
+
+			this.options = options;
+			this.settings = $.extend( {}, defaults, options );
+
+			this.defaultText = $(this.element).text();
+
+			this.keyValues = {
+				'min': 0,
+				'max': 0,
+				'isVisible': false,
+				'isFloat': false,
+				'isExecuted': false,
+				'isExecuting': false,
+				'disableOverride': this.settings.disableoverride,
+				'runonce': this.settings.runonce,
+				'decimals': this.settings.decimals,
+				'duration': this.settings.duration,
+				'delay': this.settings.delay,
+				'direction': this.settings.direction
+			};
+			this.init();
+			this.clearValues();
+		}
+
+		$.extend( Plugin.prototype, {
+			init: function() {
+				var el = this.element;
+				var self = this;
+				this.checkAttributes(el);
+
+				$(window).scroll(function() { self.isOnScreen(el); });
+				$(document).ready(function() { self.isOnScreen(el); });
+				$(window).on( "ec-element-enter-screen", {}, function(event, el) {
+					if (el === self.element && self.shouldRun()) {
+						self.keyValues.isExecuting = true;
+						self.countUp(el, self);
+					}
+				});
+
+				$(window).on( "ec-element-leave-screen", {}, function(event, el) {
+				});
+			},
+
+			checkAttributes: function(item) {
+				var attributes = {
+					'min': $(item).attr(this.settings.attrmin),
+					'max': $(item).attr(this.settings.attrmax),
+					'decimals': $(item).attr(this.settings.attrdecimals),
+					'duration': $(item).attr(this.settings.attrduration),
+					'delay': $(item).attr(this.settings.attrdelay),
+					'direction': $(item).attr(this.settings.attrdirection),
+					'runonce': $(item).attr(this.settings.attrrunonce)
+				};
+
+				for (var x in attributes) {
+					if (typeof attributes[x] !== typeof undefined && attributes[x] !== false) {
+						this.keyValues[x] = this.validate(attributes[x], x);
+					} else {
+						if (x == 'min' || x == 'max') { this.throwError('init.checkAttributes', 'attr' + x + ' attribute not found'); return false; }
+						if (x == 'decimals' && (typeof this.options.decimals === typeof undefined)) {
+							this.keyValues.min = this.setValueAttributes(attributes.min);
+							this.keyValues.max = this.setValueAttributes(attributes.max);
+						} else {
+							this.keyValues[x] = this.validate(this.settings[x], x);
+						}
+					}
+				}
+			},
+
+			validate: function(item, name) {
+				var result = item.toString().replace(' ', '').replace(',','.');
+				if (name == 'direction' && (item != 'asc' && item != 'desc')) {
+					this.throwError('init.validate', 'attr' + name + ' must be ASC or DESC');
+					return false;
+				}
+				if (!$.isNumeric(result) && (name != 'direction') && (name != 'runonce')) {
+					this.throwError('init.validate', 'attr' + name + ' is not numeric');
+					return false;
+				}
+
+				if (name == 'runonce' || name == 'disableoverride') {
+					var tmp = (item == 'true' || item == '1' || item) ? true : false;
+					if (tmp !== true && tmp !== false) {
+						this.throwError('init.validate', 'attr' + name + ' must be true or false');
+						return false;
+					}
+					result = tmp;
+				}
+				if (name == 'min' || name == 'max') this.setValueAttributes(result);
+				return result;
+			},
+
+			setValueAttributes: function(val) {
+				if (Math.floor(val) != val) {
+					this.keyValues.isFloat = true;
+					var decimalsTmp = (val.split('.')[1] || []).length;
+					if (decimalsTmp > this.keyValues.decimals) this.keyValues.decimals = decimalsTmp;
+				}
+				return val;
+			},
+
+			isOnScreen: function(el) {
+				var win = $(window);
+
+		    var viewport = {
+		        top : win.scrollTop(),
+		        left : win.scrollLeft()
+		    };
+
+		    viewport.right = viewport.left + win.width();
+		    viewport.bottom = viewport.top + win.height();
+				var bounds = { top: $(el).offset().top, bottom: $(el).offset().bottom, right: $(el).offset().right, left: $(el).offset().left };
+		    bounds.right = bounds.left + $(el).outerWidth();
+		    bounds.bottom = bounds.top + $(el).outerHeight();
+
+		    var result = (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+
+				if (result) {
+	        if (!this.keyValues.isVisible) {
+            $(window).trigger('ec-element-enter-screen', [el]);
+            this.keyValues.isVisible = true;
+	        }
+		    } else if (this.keyValues.isVisible) {
+					$(window).trigger('ec-element-leave-screen', [el]);
+	        this.keyValues.isVisible = false;
+		    }
+		    return result;
+			},
+
+			countUp: function(target, self) {
+				var min = (self.keyValues.isFloat) ? parseFloat(self.keyValues.min) : parseInt(self.keyValues.min);
+				var max = (self.keyValues.isFloat) ? parseFloat(self.keyValues.max) : parseInt(self.keyValues.max);
+				var delay = self.keyValues.delay;
+
+				if (self.keyValues.direction == 'desc') {
+					var tmp = min;
+					min = max;
+					max = tmp;
+				}
+				var t = setTimeout(function(){
+			    $({countNum: min}).stop(true, true).animate({countNum: max}, {
+			        duration: parseInt(self.keyValues.duration),
+			        easing:'linear',
+							start: function() {
+								$(window).trigger('ec-animation-start', [target]);
+								self.keyValues.isExecuting = true;
+							},
+			        step: function() {
+			            if (!self.keyValues.isFloat) $(target).text(Math.floor(this.countNum));
+			            else $(target).text(this.countNum.toFixed(self.keyValues.decimals));
+			        },
+			        complete: function() {
+			            if (!self.keyValues.isFloat) $(target).text(Math.floor(this.countNum));
+			            else $(target).text(this.countNum.toFixed(self.keyValues.decimals));
+			        },
+							done: function() {
+								$(window).trigger('ec-animation-end', [target]);
+								self.keyValues.isExecuting = false;
+								self.keyValues.isExecuted = true;
+							}
+			    });
+				}, delay);
+			},
+
+			shouldRun: function() {
+				if ((this.keyValues.runonce && !this.keyValues.isExecuted) || (!this.keyValues.isExecuting)) return true;
+				else return false;
+			},
+
+			clearValues: function() {
+				var text = (this.keyValues.direction == 'asc') ? this.keyValues.min : this.keyValues.max;
+				if ($(this.element).text().length > 0) $(this.element).text(this.defaultText);
+				if (!this.keyValues.disableOverride && !$(this.element).text()) $(this.element).text(text);
+				if (this.keyValues.disableOverride && !$(this.element).text()) $(this.element).text('&nbsp;');
+			},
+
+			throwError: function(method, msg) {
+				console.error('%cError: easyCount [' + method + '] -> ' + msg, 'background: #c0392b; color: white;');
+			},
+			throwWarning: function(method, msg) {
+				console.error('%cWarning: easyCount [' + method + '] -> ' + msg, 'background: #d35400; color: white;');
+			}
+		} );
+
+		$.fn[ pluginName ] = function( options ) {
+			return this.each( function() {
+				if ( !$.data( this, "plugin_" + pluginName ) ) {
+					$.data( this, "plugin_" +
+						pluginName, new Plugin( this, options ) );
+				}
+			} );
+		};
+
+} )( jQuery, window, document );
